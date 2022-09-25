363d5d2dda2224e1902f21f00ad8fc19ead4d217
kostyaget <progogyd@gmail.com>
2022-09-25 15:56:33 +0300

    start files v2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+function App() {
+    return (
+        <>
+          <Header/>  
+        </>
+)
+
+    // console.log(post)
+}
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div>
+            <h2>    
+                Hello
+                
+            </h2>
+        </div>
+    )
+}
