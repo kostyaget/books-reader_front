dbb27c3bca22616da01c80c7bf629aeedd56daec
Vladislav Boiko <90558354+Valdesrep@users.noreply.github.com>
2022-09-27 17:12:54 +0300

    finish modal-add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero/hero";
+// import LibraryModal from "./components/Library/LibraryModal";
 // import NumberBooks from './components/NumberBooks/NumberBooks.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      {/* <LibraryModal></LibraryModal> */}
       <Hero />
       {/* <NumberBooks/> */}
     </>
