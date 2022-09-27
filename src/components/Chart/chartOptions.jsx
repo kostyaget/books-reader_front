ee944677d96694f1ad4fb07dae94ac8d60e3e12d
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-09-27 23:31:06 +0300

    chart_beginning

---
+++ src/components/Chart/chartOptions.jsx
@@ -0,0 +1,86 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Colors } from "./Chart.styled";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  layout: {
+    padding: {
+      top: 40,
+    },
+  },
+  elements: {
+    line: {
+      tension: 0.4,
+      borderWidth: 2,
+    },
+  },
+  plugins: {
+    tooltip: {
+      intersect: false,
+      enabled: false,
+    },
+    legend: {
+      display: false,
+    },
+    customBorder: {
+      borderColor: Colors.borderLineColor,
+    },
+  },
+  scales: {
+    x: {
+      display: true,
+      grid: {
+        color: Colors.borderLineColor,
+        borderColor: Colors.borderLineColor,
+        drawTicks: false,
+      },
+      ticks: {
+        display: false,
+      },
+      title: {
+        align: "end",
+        display: true,
+        text: "ЧАС",
+        color: Colors.dark,
+        font: {
+          family: "Montserrat",
+          size: 12,
+          weight: 600,
+          lineHeight: 1.25,
+        },
+      },
+    },
+    y: {
+      display: true,
+      grace: "10%",
+      grid: {
+        display: false,
+        borderColor: Colors.borderLineColor,
+        drawTicks: false,
+      },
+      ticks: {
+        display: false,
+      },
+    },
+  },
+};
