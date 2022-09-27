ce5a53624f9d24c11103a5cccf704eb8f6545f44
Vladislav Boiko <90558354+Valdesrep@users.noreply.github.com>
2022-09-27 17:14:48 +0300

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero/hero";
-// import LibraryModal from "./components/Library/LibraryModal";
 // import NumberBooks from './components/NumberBooks/NumberBooks.jsx'
+// import LibraryModal from "./components/Library/LibraryModal";
+
 
 function App() {
   return (
     <>
       <Header />
-      {/* <LibraryModal></LibraryModal> */}
       <Hero />
       {/* <NumberBooks/> */}
     </>
