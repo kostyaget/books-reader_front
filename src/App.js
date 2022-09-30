d3c269e336984c8c81defc7f15bf3c518d998f1e
kostyaget <progogyd@gmail.com>
2022-09-30 23:55:54 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/hero";
-// import RegistrationSection from "./components/Hero/registrationForm";
 import Library from "./pages/Library/Library";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
