851cc5cf941ef30b76227b3010c1ed59660b58b6
kostyaget <progogyd@gmail.com>
2022-09-30 23:14:39 +0300

    Revert "Merge pull request #30 from Leonid578/modals"
    This reverts commit cee94134a1093a33b70e9ca768c0ef7b8fd56cbd, reversing
    changes made to 674ee28e7e40ffd698e1a3283a6928b485fa2d0b.

--- src/modal/modal.jsx
+++ src/modal/modal.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import "./modal.css";
 
 const Modal = ({ active, setActive, children }) => {
-
-  // document.addEventListener('keydown', function(e) {
-  //   if (e.key === 'Escape') {
-  //   setActive(false)
-  //   }
-  //   });
-
   return (
     <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
       <div
