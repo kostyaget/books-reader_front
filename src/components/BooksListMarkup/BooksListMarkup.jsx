cd9046efc022d250deef5907e5bd2a2e04d92432
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-10-01 21:25:18 +0300

    books list markup logic

--- src/components/BooksListMarkup/BooksListMarkup.jsx
+++ src/components/BooksListMarkup/BooksListMarkup.jsx
@@ -12,6 +12,7 @@ import {
   Tbody,
   Trbody,
   TdBookTitle,
+  TdEmpty,
   TdAuthor,
   TdYear,
   TdPages,
@@ -24,15 +25,24 @@ import {
   TdMobile,
   FirstColumn,
   IconContainer,
+  LibraryIco,
+  LibraryActiveIco,
   Text,
 } from "./BooksListMarkup.styled";
+import PropTypes from "prop-types";
 
-const raiting = true;
-
-export default function BooksListMarkup() {
+export default function BooksListMarkup({
+  titleSection = "",
+  alreadyReadList = false,
+  list = [],
+}) {
+  let empty = false;
+  if (!list.length) {
+    empty = true;
+  }
   return (
     <Wrapper>
-      <SectionTitle>Маю намір прочитати</SectionTitle>
+      {titleSection && <SectionTitle>{titleSection}</SectionTitle>}
       <Table>
         <THead>
           <TrHead>
@@ -40,62 +50,102 @@ export default function BooksListMarkup() {
             <ThAuthor>Автор</ThAuthor>
             <ThYear>Рік</ThYear>
             <ThPages>Стор.</ThPages>
-            {raiting && <ThRating>Рейтинг</ThRating>}
+            {alreadyReadList && <ThRating>Рейтинг</ThRating>}
           </TrHead>
         </THead>
         <Tbody>
-          {/* {items.map(({ id, type, amount, currency }) => ( */}
-          <Trbody>
-            <TdBookTitle>
-              <FirstColumn>
-                <IconContainer>Ico</IconContainer>
-                <div>
-                  <Text>
-                    Scrum. Революционный метод управлениями проектами.
-                  </Text>
-                  {/* mobile table start */}
-                  <MobileTableWrapper>
-                    <TableMobile>
-                      <TbodyMobile>
-                        <TrMobile>
-                          <ThMobile>Автор:</ThMobile>
-                          <TdMobile>Алекс Остервальдер, Ив Пинье</TdMobile>
-                        </TrMobile>
-                        <TrMobile>
-                          <ThMobile>Рік:</ThMobile>
-                          <TdMobile>2014</TdMobile>
-                        </TrMobile>
-                        <TrMobile>
-                          <ThMobile>Стор.:</ThMobile>
-                          <TdMobile>25</TdMobile>
-                        </TrMobile>
-                        {raiting && (
-                          <TrMobile>
-                            <ThMobile>Рейтинг:</ThMobile>
-                            <TdMobile>55555</TdMobile>
-                          </TrMobile>
-                        )}
-                      </TbodyMobile>
-                    </TableMobile>
-                  </MobileTableWrapper>
-                  {/* mobile table end */}
-                </div>
-              </FirstColumn>
-            </TdBookTitle>
-            <TdAuthor>
-              <Text>Алекс Остервальдер, Ив Пинье</Text>{" "}
-            </TdAuthor>
-            <TdYear>
-              <Text>2014</Text>
-            </TdYear>
-            <TdPages>
-              <Text>25</Text>
-            </TdPages>
-            {raiting && <TdRating>55555 </TdRating>}
-          </Trbody>
-          {/* ))} */}
+          {list.map(
+            ({
+              id,
+              alreadyRead,
+              readingNow,
+              goingToRead,
+              book_name,
+              author,
+              year,
+              pages,
+              rating,
+            }) => (
+              <Trbody key={id}>
+                <TdBookTitle>
+                  <FirstColumn>
+                    <IconContainer>
+                      {alreadyRead && <LibraryIco />}
+                      {readingNow && <LibraryActiveIco />}
+                      {goingToRead && <LibraryIco />}
+                    </IconContainer>
+                    <div>
+                      <Text>{book_name}</Text>
+                      {/* mobile table start */}
+                      <MobileTableWrapper>
+                        <TableMobile>
+                          <TbodyMobile>
+                            <TrMobile>
+                              <ThMobile>Автор:</ThMobile>
+                              <TdMobile>{author}</TdMobile>
+                            </TrMobile>
+                            <TrMobile>
+                              <ThMobile>Рік:</ThMobile>
+                              <TdMobile>{year}</TdMobile>
+                            </TrMobile>
+                            <TrMobile>
+                              <ThMobile>Стор.:</ThMobile>
+                              <TdMobile>{pages}</TdMobile>
+                            </TrMobile>
+                            {alreadyReadList && (
+                              <TrMobile>
+                                <ThMobile>Рейтинг:</ThMobile>
+                                <TdMobile>{rating}</TdMobile>
+                              </TrMobile>
+                            )}
+                          </TbodyMobile>
+                        </TableMobile>
+                      </MobileTableWrapper>
+                      {/* mobile table end */}
+                    </div>
+                  </FirstColumn>
+                </TdBookTitle>
+                <TdAuthor>{author}</TdAuthor>
+                <TdYear>{year}</TdYear>
+                <TdPages>{pages}</TdPages>
+                {alreadyReadList && <TdRating>{rating}</TdRating>}
+              </Trbody>
+            )
+          )}
+          {/* empty start */}
+          {empty && (
+            <Trbody>
+              <TdEmpty>
+                <FirstColumn>
+                  <IconContainer>
+                    <LibraryIco />
+                  </IconContainer>
+                  <div>
+                    <Text>Книг нема</Text>
+                  </div>
+                </FirstColumn>
+              </TdEmpty>
+              {/* <TdAuthor></TdAuthor>
+              <TdYear></TdYear>
+              <TdPages></TdPages> */}
+              {alreadyReadList && <TdRating></TdRating>}
+            </Trbody>
+          )}
+          {/* empty end */}
         </Tbody>
       </Table>
     </Wrapper>
   );
 }
+
+BooksListMarkup.propTypes = {
+  titleSection: PropTypes.string.isRequired,
+  alreadyReadList: PropTypes.bool.isRequired,
+  list: PropTypes.array.isRequired,
+};
+
+// how to connect
+// import BooksListMarkup from './components/BooksListMarkup/BooksListMarkup';
+// import BOOKSLIST_DATA_TEST from './components/BooksListMarkup/BOOKSLIST_DATA_TEST.json'
+
+// <BooksListMarkup titleSection={titleSection} alreadyReadList={false} list={booksListBase} />
