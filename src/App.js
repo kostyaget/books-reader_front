7639d11fc38459821f45b8b4ab0399ab4f2f15ba
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-10-03 12:31:19 +0300

    eer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@
 // import { useSelector } from "react-redux";
 // import { selectIsLoggedIn } from "./redux/auth/auth";
 
-
 // Authorization закоментированны руты, если разкометить ошибки по 45 строчке
 import Library from "./pages/Library/Library";
 import Header from "./components/Header/Header";
 import Registration from "./pages/registration/Registration";
 import Login from "./pages/login/Login";
 
-// import Hero from "./components/Hero/hero";
+import Hero from "./components/Hero/hero";
 // import RegistrationSection from "./components/Hero/hero";
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -25,6 +24,10 @@ const App = () => {
         <Route path="/login" element={<Login />}></Route>
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
+<<<<<<< Updated upstream
+=======
+      <Hero />
+>>>>>>> Stashed changes
     </>
   );
 };
