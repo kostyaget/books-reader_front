953131bbf8ceb4b7ccf668e74fd10a757648710f
Veronika Sirak <7nicka@gmail.com>
2022-10-04 13:35:01 +0100

    fix bugs

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+// import { useState } from "react";
 import ResultForm from "./ResultForm/ResultForm";
 import PagesStats from "../PagesStats/PagesStats";
 import { useFetchUserDataQuery } from "../../redux/users/usersApi";
 import { Section, Title } from "./Result.styled";
 
 export default function Result() {
-  const [isModalOpen, setModalOpen] = useState(false);
+  // const [isModalOpen, setModalOpen] = useState(false);
   const { data } = useFetchUserDataQuery();
 
-  const openModal = () => {
-    setModalOpen(!isModalOpen);
-  };
+  // const openModal = () => {
+  //   setModalOpen(!isModalOpen);
+  // };
 
   return (
     <>
