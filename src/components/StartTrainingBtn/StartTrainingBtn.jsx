4c3d11fda3df9e9cd7f416769a0fd9601236ead1
Veronika Sirak <7nicka@gmail.com>
2022-10-06 00:58:02 +0100

    add charts array

--- src/components/StartTrainingBtn/StartTrainingBtn.jsx
+++ src/components/StartTrainingBtn/StartTrainingBtn.jsx
@@ -1,8 +1,11 @@
 import { Button } from "./StartTrainingBtn.styled";
 
 export default function StartTrainingBtn({ openStatistics }) {
+  const hadleClick = () => {
+    openStatistics();
+  };
   return (
-    <Button type="submit" onClick={openStatistics}>
+    <Button type="submit" onClick={hadleClick}>
       Start traininig
     </Button>
   );
--- src/pages/Training/Training.jsx
+++ src/pages/Training/Training.jsx
@@ -10,6 +10,7 @@ import Result from "../../components/Result/Result";
 import ClockTimes from "../../components/Clock/index";
 import StartTrainingBtn from "../../components/StartTrainingBtn/StartTrainingBtn";
 import { useFetchTrainingsDataQuery } from "../../redux/trainings/trainingsApi";
+import { useFetchUserDataQuery } from "../../redux/users/usersApi";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 import {
   Section,
@@ -25,7 +26,18 @@ export default function Training() {
   const [isTrainingAddBookShown, setIsTrainingAddBookShown] = useState(false);
   const [isTrainingActive, setIsTrainingActive] = useState(false);
   const { data } = useFetchTrainingsDataQuery();
+  const userDate = useFetchUserDataQuery();
 
+  const progressDate = userDate?.data?.user?.info?.progress.map(
+    (i) => i.trainingDate
+  );
+  const progressPage = userDate?.data?.user?.info?.progress.map(
+    (e) => e.pagesAmount
+  );
+  const pageSummary = userDate?.data?.user?.info?.progress.reduce(
+    (a, e, i) => a.concat(i === 0 ? e.pagesAmount : e.pagesAmount + a.at(-1)),
+    []
+  );
   const isMobile = useMediaQuery("(max-width: 767px)");
   const isTablet = useMediaQuery("(min-width: 768px) and (max-width: 1023px)");
   const isDesktop = useMediaQuery("(min-width: 1024px)");
@@ -106,7 +118,11 @@ export default function Training() {
             <TrainingContent>
               <ClockTimes />
               <BooksListTraining />
-              <Chart />
+              <Chart
+                labelsData={progressDate}
+                actData={pageSummary}
+                planData={progressPage}
+              />
             </TrainingContent>
           </MyTrainingWarp>
           <SideBar>
