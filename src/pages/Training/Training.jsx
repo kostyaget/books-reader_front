9435baf0f5be23f1b4e7d4665c7e25ca3410cda7
Veronika Sirak <7nicka@gmail.com>
2022-10-05 14:14:20 +0100

    add btn

--- src/pages/Training/Training.jsx
+++ src/pages/Training/Training.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Chart from "../../components/Chart/Chart";
-// import WithBooks from "../../components/NumberBooks/WithBooks";
+import WithBooks from "../../components/NumberBooks/WithBooks";
 import WithoutBooks from "../../components/NumberBooks/WithoutBooks";
 import RoundButton from "../../components/RoundButton/RoundButton";
 import TrainingAddBookModal from "../../components/TrainingAddBookModal/TrainingAddBookModal";
 import MyTraining from "../../components/MyTraining/MyTraining";
 import BooksListTraining from "../../components/BooksListTraining/BooksListTraining";
-// import Result from "../../components/Result/Result";
-// import ClockTimes from "../../components/Clock/index";
+import Result from "../../components/Result/Result";
+import ClockTimes from "../../components/Clock/index";
 import StartTrainingBtn from "../../components/StartTrainingBtn/StartTrainingBtn";
 import { useFetchTrainingsDataQuery } from "../../redux/trainings/trainingsApi";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
@@ -24,6 +24,7 @@ import {
 
 export default function Training() {
   const [isTrainingAddBookShown, setIsTrainingAddBookShown] = useState(false);
+  const [isTrainingActive, setIsTrainingActive] = useState(false);
   const { data } = useFetchTrainingsDataQuery();
 
   const isMobile = useMediaQuery("(max-width: 767px)");
@@ -33,14 +34,18 @@ export default function Training() {
   const openModal = () => {
     setIsTrainingAddBookShown(!isTrainingAddBookShown);
   };
-
+  const openStatistics = () => {
+    setIsTrainingActive(!isTrainingActive);
+  };
   return (
     <Section>
-      {isMobile && (
+      {isMobile && !isTrainingActive && (
         <>
           <WithoutBooks />
           <BooksListTraining />
-          {data?.data.length > 0 && <StartTrainingBtn />}
+          {data?.data.length > 0 && (
+            <StartTrainingBtn openStatistics={openStatistics} />
+          )}
           <Chart />
           <RoundButton openModal={openModal} />
           <TrainingAddBookModal
@@ -49,7 +54,7 @@ export default function Training() {
           />
         </>
       )}
-      {/* {isMobile && data?.data.length > 0 && (
+      {isMobile && isTrainingActive && (
         <>
           <ClockTimes />
           <WithBooks />
@@ -57,18 +62,20 @@ export default function Training() {
           <Chart />
           <Result />
         </>
-      )} */}
+      )}
 
-      {isTablet && (
+      {isTablet && !isTrainingActive && (
         <>
           <WithoutBooks />
           <MyTraining />
           <BooksListTraining />
-          {data?.data.length > 0 && <StartTrainingBtn />}
+          {data?.data.length > 0 && (
+            <StartTrainingBtn openStatistics={openStatistics} />
+          )}
           <Chart />
         </>
       )}
-      {/* {isTablet && data?.data.length > 0 && (
+      {isTablet && isTrainingActive && (
         <>
           <ClockTimes />
           <WithBooks />
@@ -76,14 +83,16 @@ export default function Training() {
           <Chart />
           <Result />
         </>
-      )} */}
-      {isDesktop && (
+      )}
+      {isDesktop && !isTrainingActive && (
         <DesktopTrainingWrapper>
           <MyTrainingWarp>
             <TrainingContent>
               <MyTraining />
               <BooksListTraining />
-              {data?.data.length > 0 && <StartTrainingBtn />}
+              {data?.data.length > 0 && (
+                <StartTrainingBtn openStatistics={openStatistics} />
+              )}
               <Chart />
             </TrainingContent>
           </MyTrainingWarp>
@@ -92,7 +101,7 @@ export default function Training() {
           </SideBar>
         </DesktopTrainingWrapper>
       )}
-      {/* {isDesktop && data?.data[0].status && (
+      {isDesktop && isTrainingActive && (
         <DesktopTrainingWrapper>
           <MyTrainingWarp>
             <TrainingContent>
@@ -106,7 +115,7 @@ export default function Training() {
             <Result />
           </SideBar>
         </DesktopTrainingWrapper>
-      )} */}
+      )}
     </Section>
   );
 }
