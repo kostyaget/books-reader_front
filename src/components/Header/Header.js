707a7289441c8a3d6a31b7cd60b3b1b5a637ed1d
Vladislav Boiko <90558354+Valdesrep@users.noreply.github.com>
2022-10-05 16:20:47 +0300

    fix Routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/auth/auth";
+import { selectIsLoggedIn, selectCurrentToken } from "../../redux/auth/auth";
 import LogOutMenu from "./LogOutMenu";
 import LogInMenu from "./LogInMenu";
 import { Header } from "./Header.styled";
 
 export default function HeaderMenu() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isToken = useSelector(selectCurrentToken);
 
   return (
     <>
-      <Header>{isLoggedIn ? <LogInMenu /> : <LogOutMenu />}</Header>
+      <Header>{isLoggedIn && isToken ? <LogInMenu /> : <LogOutMenu />}</Header>
     </>
   );
 }
