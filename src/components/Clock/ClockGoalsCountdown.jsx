22514721f1773576270b8b774b5790d31739b14c
Veronika Sirak <7nicka@gmail.com>
2022-10-07 10:39:19 +0100

    add fn timer

--- src/components/Clock/ClockGoalsCountdown.jsx
+++ src/components/Clock/ClockGoalsCountdown.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { makeMeTwoDigits } from "../../utils/formatTrainigData";
 import {
   GoalContainer,
   GoalTitle,
@@ -7,19 +9,39 @@ import {
   GoalBackground,
 } from "./ClockTest";
 
-const ClockGoalsCountdown = ({ days, hours, minutes, seconds }) => {
+const ClockYearsCountdown = ({ deadline, startDate }) => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now(startDate);
+
+    setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
+  useEffect(() => {
+    const interval = setInterval(() => getTime(deadline), 1000);
+
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <GoalContainer>
-      <GoalTitle>Goals countdown</GoalTitle>
+      <GoalTitle>Years countdown</GoalTitle>
 
       <GoalBackground>
-        <GoalCountdownTime>{days}</GoalCountdownTime>
+        <GoalCountdownTime>{makeMeTwoDigits(days)}</GoalCountdownTime>
         <GoalCountdownTime>:</GoalCountdownTime>
-        <GoalCountdownTime>{hours}</GoalCountdownTime>
+        <GoalCountdownTime>{makeMeTwoDigits(hours)}</GoalCountdownTime>
         <GoalCountdownTime>:</GoalCountdownTime>
-        <GoalCountdownTime>{minutes}</GoalCountdownTime>
+        <GoalCountdownTime>{makeMeTwoDigits(minutes)}</GoalCountdownTime>
         <GoalCountdownTime>:</GoalCountdownTime>
-        <GoalCountdownTime>{seconds}</GoalCountdownTime>
+        <GoalCountdownTime>{makeMeTwoDigits(seconds)}</GoalCountdownTime>
 
         <GoalItemTime>
           <GoalText>DAYS</GoalText>
@@ -32,4 +54,4 @@ const ClockGoalsCountdown = ({ days, hours, minutes, seconds }) => {
   );
 };
 
-export default ClockGoalsCountdown;
+export default ClockYearsCountdown;
