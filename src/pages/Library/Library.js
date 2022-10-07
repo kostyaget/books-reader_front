8b468efe476b9bfebe4debdd30072ff107864531
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-10-07 07:24:34 +0300

    fix google auth

--- src/pages/Library/Library.js
+++ src/pages/Library/Library.js
@@ -1,13 +1,20 @@
 import InfoModal from "../../components/Modal/InfoModal";
 import LibraryForm from "../../components/LibraryForm/LibraryForm";
 import LibraryFormMobail from "../../components/LibraryForm/LibraryFormMobail";
-import { LibrarySection, ToFormBtn, LibraryBnt } from "./Library.styled";
+import { LibrarySection } from "./Library.styled";
 import LibraryAllCategories from "../../components/LibraryAllCategories/LibraryAllCategories";
 import { selectCurrentUserBook } from "../../redux/auth/auth";
 import { useSelector } from "react-redux";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
+import { ToFormBtn } from "../../components/LibraryForm/LibraryFormMobile.styled";
 import { ReactComponent as Plus } from "../../images/IconP.svg";
 import { useState } from "react";
+
+// import queryString from "query-string";
+// import { useLocation } from "react-router-dom";
+// import React, { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { googleLogIn } from "../../redux/auth/auth";
 
 export default function Library() {
   const [modalOpen, setModalOpen] = useState(true);
@@ -19,6 +26,17 @@ export default function Library() {
   const isTablet = useMediaQuery("(min-width: 768px)");
   const isBook = useSelector(selectCurrentUserBook);
 
+  // const location = useLocation();
+  // const query = queryString.parse(location.search);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   if (query.token) {
+  //     const { name, email, token } = query;
+  //     const user = { name, email };
+  //     dispatch(googleLogIn({ user, token }));
+  //   }
+  // });
+
   return (
     <>
       {isMobile && (
@@ -28,7 +46,6 @@ export default function Library() {
               <LibraryFormMobail onClose={toggleModal}></LibraryFormMobail>
             )}
             {isBook ? <InfoModal></InfoModal> : <LibraryAllCategories />}
-            <LibraryBnt to="/training">My training</LibraryBnt>
             <ToFormBtn onClick={toggleModal}>
               <Plus />
             </ToFormBtn>
@@ -40,7 +57,6 @@ export default function Library() {
           <LibrarySection>
             <LibraryForm></LibraryForm>
             {isBook ? <InfoModal></InfoModal> : <LibraryAllCategories />}
-            <LibraryBnt to="/training">My training</LibraryBnt>
           </LibrarySection>
         </>
       )}
